e692bc91667ad5d9031e771ca9e6d234496f4064
alony <alony@wix.com>
2018-06-18 14:10:01 +0300

    gojs show branch tree

--- visualizations/src/gojs/simple-diagram.ts
+++ visualizations/src/gojs/simple-diagram.ts
@@ -1,36 +1,39 @@
 import * as go from 'gojs';
+import { branchNode, commentNode, commitNode, link } from "./nodes-creator";
+import { linksTemplates, nodesTemplates } from "./templates";
 
 export default function simpleDiagram(root) {
+  const nodes = {
+    master: branchNode('master', { loc: go.Point.parse('0 0') }),
+    featureBranch: branchNode('feature branch', { loc: go.Point.parse('0 0') }),
+    master1: commitNode('1'),
+    master2: commitNode('2'),
+    mergeCommit: commitNode('3'),
+    ft1: commitNode('1'),
+    ft2: commitNode('2'),
+    start: commentNode('start'),
+    end: commentNode('end')
+  };
+
   const $ = go.GraphObject.make;
   const myDiagram = $(go.Diagram, root);
-  const nodeDataArray = [
-    { key: 'Alpha', color: 'lime' },
-    { key: 'Beta', color: 'cyan' },
-    { key: 'Zeta', isGroup: true },
-    { key: 'Delta', color: 'pink', group: 'Zeta' },
-    { key: 'Gamma', color: 'maroon', group: 'Zeta' }
-  ];
+
+  const nodeDataArray = Object.keys(nodes).map(key => nodes[key]);
   const linkDataArray = [
-    { to: 'Beta', from: 'Alpha', color: 'red' },
-    { to: 'Zeta', from: 'Alpha' },
-    { to: 'Zeta', from: 'Beta' }
+    link(nodes.master, nodes.master1),
+    link(nodes.master1, nodes.master2),
+    link(nodes.master2, nodes.mergeCommit),
+    link(nodes.master1, nodes.featureBranch),
+    link(nodes.featureBranch, nodes.ft1),
+    link(nodes.ft1, nodes.ft2),
+    link(nodes.ft2, nodes.mergeCommit),
+    // commentLink(nodes.start, nodes.master1),
+    // commentLink(nodes.end, nodes.ft2)
   ];
+
   myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-
-  myDiagram.add($(go.Part, 'Vertical', { background: 'red' },
-    $(go.Shape, 'RoundedRectangle', { fill: 'white' }),
-    $(go.TextBlock, 'some text', { background: 'lightgray', textAlign: 'left', stretch: go.GraphObject.Fill })
-  ));
-
-  myDiagram.nodeTemplate = $(go.Node, 'Auto',
-    $(go.Shape, 'RoundedRectangle', { fill: 'white' }, new go.Binding('fill', 'color')),
-    $(go.TextBlock, 'text', { margin: 10 }, new go.Binding('text', 'key'))
-  );
-
-  myDiagram.linkTemplate = $(go.Link,
-    $(go.Shape, { strokeWidth: 3 }, new go.Binding('stroke', 'color')),
-    $(go.Shape, { toArrow: 'Standard', stroke: 'null' }, new go.Binding('fill', 'color')),
-    $(go.Shape, { fromArrow: 'Standard', stroke: 'null' }, new go.Binding('fill', 'color'))
-  );
+  myDiagram.nodeTemplateMap = nodesTemplates;
+  myDiagram.linkTemplateMap = linksTemplates;
+  myDiagram.layout = $(go.TreeLayout, { angle: 90 });
   return myDiagram;
 }
