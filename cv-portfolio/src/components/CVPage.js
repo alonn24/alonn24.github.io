9912921ceed35fe214cbd819eb80760a18be8bdb
Alon Yehezkel <alony@bookaway.com>
2021-11-20 11:29:57 +0200

    Rainy background

--- cv-portfolio/src/components/CVPage.js
+++ cv-portfolio/src/components/CVPage.js
@@ -1,4 +1,4 @@
-import { VStack } from "@chakra-ui/react";
+import { VStack, Box } from "@chakra-ui/react";
 import { useColorModeValue } from '@chakra-ui/color-mode';
 import Toolbar from './sections/Toolbar';
 import Section from './layout/Section';
@@ -7,27 +7,31 @@ import Education from "./sections/Education";
 import Experience from "./sections/Experience";
 import Profile from "./sections/Profile";
 import Hero from "./layout/Hero";
+import RainyBackground from "./RainyBackground";
 
 function CVPage() {
   const value = useColorModeValue('colors.primary.300', 'colors.primary.300');
   const inverseColor = useColorModeValue('colors.gray.800', 'colors.gray.800');
 
   const sx = { "--contact-border-color": value, "--text-color-inverse": inverseColor };
-  return <VStack alignItems="stretch" sx={sx}>
-    <Hero>
-      <Profile as="h2" />
-      <Section as="h1" flex="1" title="About me" mt={{ base: '1rem' }}>
-        <AboutMe />
+  return <>
+    <RainyBackground />
+    <VStack alignItems="stretch" sx={sx}>
+      <Hero>
+        <Profile as="h2" />
+        <Section as="h1" flex="1" title="About me" mt={{ base: '1rem' }}>
+          <AboutMe />
+        </Section>
+      </Hero>
+      <Section as="h1" title="Experience">
+        <Experience />
       </Section>
-    </Hero>
-    <Section as="h1" title="Experience">
-      <Experience />
-    </Section>
-    <Section as="h1" title="Education">
-      <Education />
-    </Section>
-    <Toolbar />
-  </VStack>
+      <Section as="h1" title="Education">
+        <Education />
+      </Section>
+      <Toolbar />
+    </VStack>
+  </>
 }
 
 export default CVPage;
---
+++ cv-portfolio/src/components/RainyBackground.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+const rainColors = ["rgba(49, 130, 206, 0.8)", "rgba(246, 173, 85, 0.8)"];
+const ExperienceCanvas = () => {
+  const canvasRef = React.useRef();
+
+  React.useEffect(() => {
+    const canvas = canvasRef.current;
+    const canvasContainer = canvas.parentNode;
+    canvas.width = canvasContainer.clientWidth;
+    canvas.height = canvasContainer.clientHeight;
+    var ctx = canvas.getContext('2d');
+    var w = canvas.width;
+    var h = canvas.height;
+
+    // ctx.strokeStyle = value;
+    // ctx.lineWidth = 3;
+
+    // Initialize maxParts drops
+    var particles = [];
+    var maxParts = 50;
+    for(var a = 0; a < maxParts; a++) {
+      particles.push({
+        x: Math.random() * w,
+        y: Math.random() * h,
+        l: Math.random() * 1,
+        xs: -5 + Math.random() * 10,
+        ys: Math.random() * 10 + 10,
+        c: rainColors[parseInt(Math.random() * 2)],
+        lw: Math.random() * 5
+      })
+    }
+
+    function draw() {
+      ctx.clearRect(0, 0, w, h);
+      for (var c = 0; c < particles.length; c++) {
+        // draw
+        var p = particles[c];
+        ctx.beginPath();
+        ctx.moveTo(p.x, p.y);
+        ctx.lineTo(p.x + p.l * p.xs, p.y + p.l * p.ys);
+        ctx.strokeStyle = p.c;
+        ctx.lineWidth = p.lw;
+        ctx.stroke();
+
+        // move
+        p.x += p.xs;
+        p.y += p.ys;
+        if (p.x > w || p.y > h) {
+          p.x = Math.random() * w;
+          p.y = -20;
+        }
+      }
+    }
+
+    const intervalID = setInterval(draw, 30);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, []);
+
+  const styles = { position: 'absolute', left: 0, width: '100%', top: 0, height: '100%' };
+  return <canvas ref={canvasRef} style={styles} />
+};
+
+export default ExperienceCanvas;
