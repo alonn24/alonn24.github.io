4261dcf549501ec75fd9be306a60fd5a61bf0cdd
Alon Yehezkel <alony@bookaway.com>
2022-02-26 08:13:22 +0200

    add cloudfront tf

--- cv-portfolio/infra/README.md
+++ cv-portfolio/infra/README.md
@@ -1,3 +1,4 @@
 terraform init
 terraform import aws_s3_bucket.b alony-cv-static
+terraform import aws_cloudfront_distribution.s3_distribution E1Z3HT1LMKQLE7
 aws s3 sync build s3://alony-cv-static/c9f308bce5b045f1c2af1d3efb113b3f3d54a92f
--- cv-portfolio/infra/alony.info.tf
+++ cv-portfolio/infra/alony.info.tf
@@ -29,3 +29,54 @@ POLICY
     error_document = "index.html"
   }
 }
+
+locals {
+  s3_origin_id = "alony-cv-static.amazonaws.com"
+  s3_origin_path = "/2d527b63b84341aeaeb18f649ba99c5e0a4e38a0"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    origin_id   = local.s3_origin_id
+    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
+    origin_path = local.s3_origin_path
+    connection_attempts = 3
+    connection_timeout = 10
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    compress = true
+    target_origin_id = local.s3_origin_id
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:021468386450:certificate/d75bce37-eb7e-4a91-a968-d3e93d6f561f"
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+    cloudfront_default_certificate = false
+  }
+}
