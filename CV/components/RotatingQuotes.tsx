f157cf207ad9ca7d8fa370e59323021cfd0e650b
Alony <alonn24@gmail.com>
2025-02-09 11:23:54 +0200

    Add publications

--- CV/components/RotatingQuotes.tsx
+++ CV/components/RotatingQuotes.tsx
@@ -2,19 +2,7 @@
 
 import { useState, useEffect } from "react"
 import { motion, AnimatePresence } from "framer-motion"
-
-const sentences = [
-  "Keep pursuing knowledge, for life is an endless teacher.",
-  "Always seek new knowledge, as life's lessons are perpetual.",
-  "Embrace lifelong learning, as life is an eternal educator.",
-  "Stay committed to learning, for life's teachings are constant.",
-  "Never cease learning, as life's lessons are unending.",
-  "Maintain a thirst for knowledge, as life's wisdom is infinite.",
-  "Keep expanding your horizons, for life's education is ongoing.",
-  "Embrace continuous learning, as life never stops imparting wisdom.",
-  "Stay curious and open-minded, for life is a perpetual source of learning.",
-  "Commit to lifelong growth, as life's lessons are never-ending.",
-]
+import { sentences } from "@/data/rotatingQuotesData"
 
 const RotatingQuotes = () => {
   const [currentQuote, setCurrentQuote] = useState(0)
---
+++ CV/data/educationData.ts
@@ -0,0 +1,23 @@
+export const educationData = {
+  timeline: [
+    {
+      organization: "The Open University, Israel",
+      title: "B.A., Computer Science - Systems & Applications.",
+      start: "2006",
+      end: "2014",
+    },
+    {
+      organization: "Mamram",
+      title: "Computer Science",
+      start: "2006",
+      end: "2006",
+    },
+    {
+      organization: "Makif D, Ashdod",
+      title: "High School, Computer technology / Computer Systems Technology",
+      start: "1999",
+      end: "2005",
+    },
+  ],
+}
+
